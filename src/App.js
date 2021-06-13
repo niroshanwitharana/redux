2341ad2f2cad8c300efe47296d855b203b97afe7
niroshanwitharana <nrosh_uoc@yahoo.com>
2021-06-13 14:03:04 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import HelloWorld from './HelloWorld';
+import { createStore } from "redux";
+import reducer from "./reducers";
 
-function App() {
+const initialState = {tech : "Redux"};
+const store = createStore(reducer, initialState);
+
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HelloWorld setTech={store.getState().tech} />
     </div>
-  );
+  )
 }
 
-export default App;
---
+++ src/HelloWorld.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HelloWorld(props) {
+    return (
+        <div>
+            <h1>Hello {props.setTech}</h1>
+        </div>
+    )
+}
